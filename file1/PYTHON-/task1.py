6b19566d6f5647dd09029cca89da7fa23165202d
DEVENDHAR1408 <devendharbongiri@gmail.com>
2025-08-12 12:38:01 +0530

    newmsg

---
+++ file1/PYTHON-/task1.py
@@ -0,0 +1,228 @@
+#word="devendhar is good boy"
+#char_count=0
+#for i in range(len(word)):
+ #   if word[i]!=" ":
+ #        char_count+=1 
+#print(char_count)
+#vowels="aeiou"
+#name="Devendhar"
+#for i in range(len(name)):
+   # C=name[i]
+    #if C == "a" or C == "e" or C == "i" or  C == "o" or  C == "u":
+        #print(C, "is in","index") 
+#vowels="aeiou"
+#name="Devendhar"
+#consonents_count=0
+##   C=name[i]
+  #  if C != "a" and C != "e" and C != "i" and  C != "o" and  C != "u":
+   #     consonents_count+=1
+    #    print(C, "is in", i ,"index") 
+# name="DEVENDHAR"
+# name=name.lower()
+# print(name)
+# name1="devendhar bonagiri"
+# name2=name1.upper( )
+# print(name2) 
+# username=input("enter your name ")
+# username=username.strip()
+# print(username,len(username)) 
+# print(username =="mani")  
+# name="  devendhar "
+# name=name.rstrip()
+# print(name)
+# name ="Bonagiri Devendhar"
+# print(name)
+# name ="Bonagiri Devendhar"
+# print(name[0])
+# name ="Bonagiri Devendhar"
+# name2=len(name)-1
+# print(name[name2])
+# program="GOOD" +" " +"MORNING"
+# print(program)
+# text="webprogramming"
+# print(text * 3)
+# text="I Hope Everyone is Fine" 
+# print(text[:6])
+# word="find the length of string"
+# word=len(word)
+# print(word)
+# country ="india"
+# country = country.upper()
+# print(country)
+# country ="INDIA"
+# country = country.lower()
+# print(country)
+# behaviour="genuine"
+# behaviour=behaviour.capitalize()
+# print(behaviour)
+# sentence="everyone is happy when rcb won the cup"
+# sentence=sentence.title()
+# print(sentence)
+# name="  devendhar bonagiri   "
+# name=name.strip()
+# print(name)
+# name="devendhar bonagiri    "
+#  name=name.rstrip()
+ #  print(name)
+# name="    devendhar bonagiri"
+# name=name.lstrip()
+# print(name)
+# sentence="Iam learning PYTHON"
+# for index in range(len(sentence)):
+#     c=sentence[index]
+# if (c=="a" or  c=="e" or c=="i" or c=="o" or c=="u" ) and (index%2==0):
+#  print(c,index)
+# sentence = "I am learning python "
+# for index in range (len (sentence)):
+#    c=sentence[index]
+#    if(c == "a" or c == "e" or c == "i" or c == "o" or c =="u") and (index%2==0):
+#       print(c,index)
+# sentence = "I am learning python "
+# print(len(sentence))
+# sentence="aishwarya"
+# for i in range (len(sentence)):
+#     if sentence.count(sentence[i])==1:
+#         print(sentence[i])
+# word="i woke early in the morning"
+# print(word.split("i"))
+# num_list=["hi","how","a""r""e","you"]
+# print(" ".join(num_list))
+# name="chitti"
+# secret=" "
+# for i in range(len(name)):
+#     code=ord(name[i])
+#     new_char=chr(code+1)
+#     secret+=new_char
+#     print(secret)
+# sentence="Ee nagarAHiki Emaindhi"
+# upper_count=0
+# for i in range(len(sentence)):
+#     code=ord(sentence[i])
+#     if code>=65 and code<=90:
+#         upper_count+=1
+# print("there are",upper_count,"uppercase characters")
+# if 0:
+#   print("it is true")
+# else  
+#   print ("it is not zero")
+# name= "bonagiri Devendhar"
+# print(name[0])
+# name="Bonagiri Devendhar"
+# name2=len(name)-1
+# print(name[name2])
+# word="Everyone is playing in the ground but no one utlizing the ground properly while the ground is so good"
+# word=word.find("ground")
+# print(word)
+# sentence=" nothing is permanent "
+# sentence=sentence.rstrip( )
+# print(sentence.replace(" ","_"))
+# name=input()
+# name2=input()
+# print("BONAGIRI" if name==name2 else "is not your name")
+name="devendhar"
+print(len(name))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
